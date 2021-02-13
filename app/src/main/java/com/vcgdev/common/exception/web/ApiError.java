2f522e2ee841c5fc94ad3e2de695f8a940fd8930
vcgdev <vcg_cruz@hotmail.com>
2021-02-13 13:42:57 -0600

    update ApiError container

--- app/src/main/java/com/vcgdev/common/exception/web/ApiError.java
+++ app/src/main/java/com/vcgdev/common/exception/web/ApiError.java
@@ -1,24 +1,44 @@
 package com.vcgdev.common.exception.web;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.vcgdev.common.exception.ErrorCode;
 import com.vcgdev.common.exception.ErrorDetail;
 
 public class ApiError {
-    private ErrorCode code;
+    private String code;
+    private String message;
 	private List<ErrorDetail> errors;
 	
+    public ApiError(ErrorCode errorCode) {
+        this.code = errorCode.getCode().toString();
+        this.message = errorCode.getMessage();
+        this.errors = new ArrayList<>();
+    }
+
+    public ApiError() {
+        this.errors = new ArrayList<>();
+    }
+
 	public void addError(ErrorDetail detail) {
 		errors.add(detail);
     }
-    
-    public ErrorCode getCode() {
+
+    public String getCode() {
         return code;
     }
 
-    public void setCode(ErrorCode code) {
+    public String getMessage() {
+        return message;
+    }
+
+    public void setCode(String code) {
         this.code = code;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
     }
 
     public List<ErrorDetail> getErrors() {
